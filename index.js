f1f116ec738d3b69e05b977892d05b7951073145
ketantb <kbhatkhore777@gmail.com>
2023-02-20 22:43:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
   app.use(cors());
 
 
-require('./connectionDB.js')
+require('./connectiondb.js')
 require('./Order/orderModel/ordermodel')
 require('./Register/RegisterModel/registerModel')
 require('./SignIn/signInModel/signInModel')
